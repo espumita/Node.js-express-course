44fa161370429b78603c02bdb18fd44bbffbda63
David Falcón Naranjo <david_el_125@hotmail.com>
2016-03-24 00:15:04 +0000

    MongoDB select many and one

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,25 +1,37 @@
 var express = require('express');
 var bookRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 
 var router = function (nav) {
-
     bookRouter.route('/')
         .get(function (req,res) {
-            
-            res.render('bookListView',{
-                title: 'Books',
-                nav: nav,
-                books: books
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books');
+                collection.find({}).toArray(function (err, results) {
+                    res.render('bookListView',{
+                        title: 'Books',
+                        nav: nav,
+                        books: results
+                    });
+                });
             });
         });
 
     bookRouter.route('/:id')
         .get(function (req,res) {
-            var id = req.param('id');
-            res.render('bookView',{
-                title: 'Books',
-                nav: nav,
-                book: books[id]
+            var id = new objectId(req.param('id'));
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books');
+                collection.findOne({_id: id}, function (err, results) {
+                    res.render('bookView',{
+                        title: 'Books',
+                        nav: nav,
+                        book: results
+                    });
+                });
             });
         });
 
